010e5c28f5c55c4c4086d637c2b691fd72af2a3e
Sam Prokopchuk <ssprokopchuk@gmail.com>
2022-09-02 19:37:40 -0400

    Update setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,4 +1,3 @@
-alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
 echo "alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'" >> $HOME/.aliases.zsh
 
