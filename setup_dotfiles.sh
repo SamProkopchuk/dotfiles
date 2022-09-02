4d28a7929b79faaf6e6c5a221edcdcb7c71c1098
Sam Prokopchuk <ssprokopchuk@gmail.com>
2022-09-02 19:04:51 -0400

    Add setup_dotfiles.sh

---
+++ setup_dotfiles.sh
@@ -0,0 +1,14 @@
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+echo "alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'" >> $HOME/.zshrc
+
+requirements=( zsh wezterm nvim )
+
+for cmd in "${requirements[@]}"
+do
+    if ! command -v $cmd &> /dev/null;
+    then
+        echo "$cmd could not be found, please install it and ensure it's in your path."
+        exit
+    fi
+done
