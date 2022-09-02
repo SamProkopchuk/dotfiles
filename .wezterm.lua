2da70bbf7afdcb5e8d8d0e1b5de0721069812d75
Sam Prokopchuk <ssprokopchuk@gmail.com>
2022-09-02 19:09:26 -0400

    Add more dotfiles

---
+++ .wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font('Iosevka Curly Light'),
+  default_cursor_style = 'BlinkingBar',
+  cursor_blink_rate = 469,
+  cursor_blink_ease_in = 'Constant',
+  cursor_blink_ease_out = 'Constant', 
+  color_scheme = 'terafox',
+  adjust_window_size_when_changing_font_size = false,
+}
+
