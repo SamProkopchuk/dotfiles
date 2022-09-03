3909c90a2d655494abc6bf5d3fd4ff0ba01f7f37
Sam Prokopchuk <ssprokopchuk@gmail.com>
2022-09-02 20:13:04 -0400

    Add alises.zsh

--- .aliases.zsh
+++ .aliases.zsh
@@ -2,6 +2,3 @@ alias v=nvim
 alias vim=nvim
 alias config='/usr/bin/git --git-dir=/Users/samprokopchuk/dotfiles/ --work-tree=/Users/samprokopchuk'
 
-alias config='/usr/bin/git --git-dir=/Users/samprokopchuk/dotfiles/ --work-tree=/Users/samprokopchuk'
-alias config='/usr/bin/git --git-dir=/Users/samprokopchuk/dotfiles/ --work-tree=/Users/samprokopchuk'
-alias config='/usr/bin/git --git-dir=/Users/samprokopchuk/dotfiles/ --work-tree=/Users/samprokopchuk'
