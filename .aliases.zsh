8ea97cb792b61c9168b756df09375f597aae7422
Sam Prokopchuk <ssprokopchuk@gmail.com>
2022-09-02 21:43:47 -0400

    Update .aliases.zsh

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,4 +1,4 @@
 alias v=nvim
 alias vim=nvim
-alias config='/usr/bin/git --git-dir=/Users/samprokopchuk/dotfiles/ --work-tree=/Users/samprokopchuk'
+alias config='/usr/bin/git --git-dir="$HOME/dotfiles/" --work-tree="$HOME"'
 
