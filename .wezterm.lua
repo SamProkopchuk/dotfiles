8e9febac642f325addf5f29ae83923c7d583c545
Sam Prokopchuk <ssprokopchuk@gmail.com>
2022-09-04 03:15:58 -0400

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,14 +1,14 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-  font = wezterm.font('Iosevka Curly Light'),
-  font_size = 16.0,
-  default_cursor_style = 'BlinkingBar',
-  cursor_blink_rate = 469,
-  cursor_blink_ease_in = 'Constant',
-  cursor_blink_ease_out = 'Constant', 
-  color_scheme = 'carbonfox',
-  adjust_window_size_when_changing_font_size = false,
-  warn_about_missing_glyphs = false,
+    font = wezterm.font("Iosevka Curly Light"),
+    font_size = 16.0,
+    default_cursor_style = "BlinkingBar",
+    cursor_blink_rate = 469,
+    cursor_blink_ease_in = "Constant",
+    cursor_blink_ease_out = "Constant",
+    color_scheme = "carbonfox",
+    adjust_window_size_when_changing_font_size = false,
+    warn_about_missing_glyphs = false
 }
 
