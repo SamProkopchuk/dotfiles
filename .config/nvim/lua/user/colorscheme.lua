a7e2b975c9f099adc7c778f19d6ef5289fb376d4
Sam Prokopchuk <ssprokopchuk@gmail.com>
2022-09-09 10:19:36 -0400

    Update dotfiles

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,9 +1,5 @@
 local colorscheme = "onenord"
 
-vim.g.onenord_style = "day"
-
--- vim.o.background = "light"
-
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
