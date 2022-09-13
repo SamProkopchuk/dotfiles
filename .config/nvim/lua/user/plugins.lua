5fa5d32304aac81d433c67de8a2352cdb36e6a92
Sam Prokopchuk <ssprokopchuk@gmail.com>
2022-09-13 10:55:12 -0400

    Update neovim config

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -3,16 +3,12 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    PACKER_BOOTSTRAP = fn.system {
+        "git", "clone", "--depth", "1",
+        "https://github.com/wbthomason/packer.nvim", install_path
+    }
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -25,50 +21,50 @@ vim.cmd [[
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
+if not status_ok then return end
 
 -- Have packer use a popup window
 packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
+    display = {
+        open_fn = function()
+            return require("packer.util").float {border = "rounded"}
+        end
+    }
 }
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "neovim/nvim-lspconfig" -- Configurations for Nvim LSP
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  use { "ellisonleao/gruvbox.nvim" }
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  }
-  use "karb94/neoscroll.nvim" -- smooth scrolling plugin
-  use {
-    "kyazdani42/nvim-tree.lua",
-    requires = {
-      "kyazdani42/nvim-web-devicons", -- optional, for file icons
-    },
-    tag = "nightly" -- optional, updated every week. (see issue #1193)
-  }
-  use "ms-jpq/coq.artifacts"
-  use "ms-jpq/coq_nvim"
-  use "tpope/vim-fugitive"
-  use "sbdchd/neoformat"
-  use {
-    "nvim-telescope/telescope.nvim", tag = "0.1.x",
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+    -- My plugins here
+    use {"wbthomason/packer.nvim"} -- Have packer manage itself
+    use {
+        "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig"
+    }
+    -- use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+    use {"ellisonleao/gruvbox.nvim"}
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            require('nvim-treesitter.install').update({with_sync = true})
+        end
+    }
+    use {"karb94/neoscroll.nvim"} -- smooth scrolling plugin
+    use {
+        "kyazdani42/nvim-tree.lua",
+        requires = {"kyazdani42/nvim-web-devicons"},
+        tag = "nightly" -- optional, updated every week. (see issue #1193)
+    }
+    use {"ms-jpq/coq_nvim", requires = {"ms-jpq/coq.artifacts"}}
+    use {"tpope/vim-fugitive"}
+    use {"sbdchd/neoformat"}
+    use {
+        "ibhagwan/fzf-lua",
+        requires = {
+            "kyazdani42/nvim-web-devicons",
+            {"junegunn/fzf", run = "./install --bin"}
+        }
+    }
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then require("packer").sync() end
 end)
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -84,10 +84,30 @@ _G.packer_plugins = {
     path = "/Users/samprokopchuk/.local/share/nvim/site/pack/packer/start/coq_nvim",
     url = "https://github.com/ms-jpq/coq_nvim"
   },
+  fzf = {
+    loaded = true,
+    path = "/Users/samprokopchuk/.local/share/nvim/site/pack/packer/start/fzf",
+    url = "https://github.com/junegunn/fzf"
+  },
+  ["fzf-lua"] = {
+    loaded = true,
+    path = "/Users/samprokopchuk/.local/share/nvim/site/pack/packer/start/fzf-lua",
+    url = "https://github.com/ibhagwan/fzf-lua"
+  },
   ["gruvbox.nvim"] = {
     loaded = true,
     path = "/Users/samprokopchuk/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
     url = "https://github.com/ellisonleao/gruvbox.nvim"
+  },
+  ["mason-lspconfig.nvim"] = {
+    loaded = true,
+    path = "/Users/samprokopchuk/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
+  ["mason.nvim"] = {
+    loaded = true,
+    path = "/Users/samprokopchuk/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
   },
   neoformat = {
     loaded = true,
@@ -123,21 +143,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/samprokopchuk/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
-  },
-  ["plenary.nvim"] = {
-    loaded = true,
-    path = "/Users/samprokopchuk/.local/share/nvim/site/pack/packer/start/plenary.nvim",
-    url = "https://github.com/nvim-lua/plenary.nvim"
-  },
-  ["popup.nvim"] = {
-    loaded = true,
-    path = "/Users/samprokopchuk/.local/share/nvim/site/pack/packer/start/popup.nvim",
-    url = "https://github.com/nvim-lua/popup.nvim"
-  },
-  ["telescope.nvim"] = {
-    loaded = true,
-    path = "/Users/samprokopchuk/.local/share/nvim/site/pack/packer/start/telescope.nvim",
-    url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-fugitive"] = {
     loaded = true,
