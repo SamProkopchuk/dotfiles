77552f9b672f7d5defc9ef703c4db5469a1f05d9
Sam Prokopchuk <ssprokopchuk@gmail.com>
2024-01-23 23:58:03 -0500

    Use dragon variant of kanagawa theme.

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,8 +1,11 @@
 local colorscheme = "kanagawa"
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
+require(colorscheme).setup({
+    background = {
+        dark = "dragon",
+        light = "lotus"
+    },
+})
+
+vim.cmd("colorscheme " .. colorscheme)
 
