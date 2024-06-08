f9208f4ef92c2d8afce37e46640aeaa857697cc8
Sam Prokopchuk <ssprokopchuk@gmail.com>
2024-06-08 00:35:57 -0700

    Update colorscheme config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
     name = "gruvbox-material",
     lazy = false,
     config = function()
+      vim.g.gruvbox_material_background = "hard"
       vim.cmd("colorscheme " .. "gruvbox-material")
     end,
   },
