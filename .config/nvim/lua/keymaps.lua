ae6162bc8a498f4c4952e839e76b01d4a7da1330
Sam Prokopchuk <ssprokopchuk@gmail.com>
2024-09-18 14:00:29 -0700

    Use updated neoscroll api

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -113,3 +113,35 @@ map(
 
 -- Aerial keymaps
 map("n", "<leader>a", "<CMD>AerialToggle!<CR>")
+
+-- Neoscroll keymaps
+neoscroll = require("neoscroll")
+
+local keymap = {
+  ["<C-u>"] = function()
+    neoscroll.ctrl_u({ duration = 100, easing = "sine" })
+  end,
+  ["<C-d>"] = function()
+    neoscroll.ctrl_d({ duration = 100, easing = "sine" })
+  end,
+  ["<C-y>"] = function()
+    neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
+  end,
+  ["<C-e>"] = function()
+    neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
+  end,
+  ["zt"] = function()
+    neoscroll.zt({ duration = 100 })
+  end,
+  ["zz"] = function()
+    neoscroll.zz({ duration = 100 })
+  end,
+  ["zb"] = function()
+    neoscroll.zb({ duration = 100 })
+  end,
+}
+
+local modes = { "n", "v", "x" }
+for key, func in pairs(keymap) do
+  map(modes, key, func)
+end
--- .config/nvim/lua/plugins/neoscroll.lua
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -5,17 +5,6 @@ return {
     event = "BufWinEnter",
     config = function()
       require("neoscroll").setup({
-        -- All these keys will be mapped to their corresponding default scrolling animation
-        mappings = {
-          "<C-u>",
-          "<C-d>",
-          "<C-b>",
-          "<C-y>",
-          "<C-e>",
-          "zt",
-          "zz",
-          "zb",
-        },
         hide_cursor = true, -- Hide cursor while scrolling
         stop_eof = true, -- Stop at <EOF> when scrolling downwards
         respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
@@ -25,26 +14,6 @@ return {
         post_hook = nil, -- Function to run after the scrolling animation ends
         performance_mode = false, -- Disable "Performance Mode" on all buffers.
       })
-
-      local t = {}
-      -- Syntax: t[keys] = {function, {function arguments}}
-      -- Use the "sine" easing function
-      t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "100", [["sine"]] } }
-      t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100", [["sine"]] } }
-      -- Use the "circular" easing function
-      t["<C-b>"] = {
-        "scroll",
-        { "-vim.api.nvim_win_get_height(0)", "true", "200", [["sine"]] },
-      }
-      -- Pass "nil" to disable the easing animation (constant scrolling speed)
-      t["<C-y>"] = { "scroll", { "-0.10", "false", "100", nil } }
-      t["<C-e>"] = { "scroll", { "0.10", "false", "100", nil } }
-      -- When no easing function is provided the default easing function (in this case "quadratic") will be used
-      t["zt"] = { "zt", { "100" } }
-      t["zz"] = { "zz", { "100" } }
-      t["zb"] = { "zb", { "100" } }
-
-      require("neoscroll.config").set_mappings(t)
     end,
   },
 }
