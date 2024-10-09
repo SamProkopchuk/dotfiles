4eeecfc8d9433f472bd61489f5cdaf1890216716
Sam Prokopchuk <ssprokopchuk@gmail.com>
2024-10-09 12:17:00 -0700

    Remove deprecated thing

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -18,12 +18,6 @@ return {
           end,
           diagnostics = "nvim_lsp",
           always_show_bufferline = false,
-          diagnostics_indicator = function(_, _, diag)
-            local icons = require("lazyvim.config").icons.diagnostics
-            local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-              .. (diag.warning and icons.Warn .. diag.warning or "")
-            return vim.trim(ret)
-          end,
           offsets = {
             {
               filetype = "NvimTree",
