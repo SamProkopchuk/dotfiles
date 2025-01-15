44077735ca082ef4b04c00252b4e5e70a8c2ce2e
Sam Prokopchuk <ssprokopchuk@gmail.com>
2025-01-14 20:54:09 -0800

    Revert to gruvbox material nvim colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,33 +1,11 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    "sainnhe/gruvbox-material",
+    name = "gruvbox-material",
     lazy = false,
     config = function()
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "hard", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-      vim.cmd("colorscheme " .. "gruvbox")
+      vim.g.gruvbox_material_background = "hard"
+      vim.cmd("colorscheme " .. "gruvbox-material")
     end,
   },
 }
