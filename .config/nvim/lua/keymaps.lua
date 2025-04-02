1eec2af03addf385d0385a4e3358cbddb6c9e079
Sam Prokopchuk <ssprokopchuk@gmail.com>
2025-04-02 15:30:31 -0700

    Remove neoscroll, use codeium

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -103,45 +103,5 @@ map("n", "<C-x>", find_files_from_project_git_root)
 map("n", "<Tab>", "V:Format<CR>")
 map("v", "<Tab>", ":Format<CR>")
 
--- Copilot keymaps
-map(
-  "i",
-  "<Tab>",
-  "copilot#Accept('<CR>')",
-  { expr = true, script = true, noremap = true, silent = true, replace_keycodes = false }
-)
-
 -- Aerial keymaps
 map("n", "<leader>a", "<CMD>AerialToggle!<CR>")
-
--- Neoscroll keymaps
-neoscroll = require("neoscroll")
-
-local keymap = {
-  ["<C-u>"] = function()
-    neoscroll.ctrl_u({ duration = 100, easing = "sine" })
-  end,
-  ["<C-d>"] = function()
-    neoscroll.ctrl_d({ duration = 100, easing = "sine" })
-  end,
-  ["<C-y>"] = function()
-    neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
-  end,
-  ["<C-e>"] = function()
-    neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
-  end,
-  ["zt"] = function()
-    neoscroll.zt({ duration = 100 })
-  end,
-  ["zz"] = function()
-    neoscroll.zz({ duration = 100 })
-  end,
-  ["zb"] = function()
-    neoscroll.zb({ duration = 100 })
-  end,
-}
-
-local modes = { "n", "v", "x" }
-for key, func in pairs(keymap) do
-  map(modes, key, func)
-end
