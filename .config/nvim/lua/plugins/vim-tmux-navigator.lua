505cfb7efdece4266456278b4b705625d9495b99
Sam Prokopchuk <ssprokopchuk@gmail.com>
2025-09-29 15:43:52 -0700

    Add keymaps for navigating outside of terminal mode

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -9,11 +9,18 @@ return {
       "TmuxNavigatePrevious",
     },
     keys = {
+      -- Normal/Visual/Insert mode mappings
       { "<C-h>", "<CMD><C-U>TmuxNavigateLeft<CR>" },
       { "<C-j>", "<CMD><C-U>TmuxNavigateDown<CR>" },
       { "<C-k>", "<CMD><C-U>TmuxNavigateUp<CR>" },
       { "<C-l>", "<CMD><C-U>TmuxNavigateRight<CR>" },
       { "<C-\\>", "<CMD><C-U>TmuxNavigatePrevious<CR>" },
+      -- Terminal mode mappings
+      { "<C-h>", "<C-\\><C-N><CMD>TmuxNavigateLeft<CR>", mode = "t" },
+      { "<C-j>", "<C-\\><C-N><CMD>TmuxNavigateDown<CR>", mode = "t" },
+      { "<C-k>", "<C-\\><C-N><CMD>TmuxNavigateUp<CR>", mode = "t" },
+      { "<C-l>", "<C-\\><C-N><CMD>TmuxNavigateRight<CR>", mode = "t" },
+      { "<C-\\>", "<C-\\><C-N><CMD>TmuxNavigatePrevious<CR>", mode = "t" },
     },
   },
 }
