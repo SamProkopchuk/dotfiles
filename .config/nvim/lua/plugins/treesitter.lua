c6d2e0612738ee065f9f73492076e7710cfb762c
Sam Prokopchuk <ssprokopchuk@gmail.com>
2025-09-29 11:38:41 -0700

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,26 @@
 return {
-   {
-     "nvim-treesitter/nvim-treesitter",
-     branch = "main",
-     lazy = false,
-     config = function()
-       require("nvim-treesitter.config").setup({  -- Changed from .configs to .config
-         ensure_installed = "all",
-         sync_install = false,
-         ignore_install = { "phpdoc", "tree-sitter-phpdoc" },
-         auto_install = false,
-         highlight = {
-           enable = true,
-           disable = { "phpdoc", "tree-sitter-phpdoc" },
-           additional_vim_regex_highlighting = false,
-         },
-         rainbow = {
-           enable = true,
-           extended_mode = true,
-           max_file_lines = nil,
-         },
-         context_commentstring = { enable = true, enable_autocmd = false },
-       })
-     end,
-   },
- }
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    config = function()
+      require("nvim-treesitter.config").setup({ -- Changed from .configs to .config
+        ensure_installed = "all",
+        sync_install = false,
+        ignore_install = { "phpdoc", "tree-sitter-phpdoc" },
+        auto_install = false,
+        highlight = {
+          enable = true,
+          disable = { "phpdoc", "tree-sitter-phpdoc" },
+          additional_vim_regex_highlighting = false,
+        },
+        rainbow = {
+          enable = true,
+          extended_mode = true,
+          max_file_lines = nil,
+        },
+        context_commentstring = { enable = true, enable_autocmd = false },
+      })
+    end,
+  },
+}
