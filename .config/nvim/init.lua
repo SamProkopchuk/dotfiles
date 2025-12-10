8b4782c0a66230ec9e061c1a10c1ee45aa1e477d
Sam Prokopchuk <ssprokopchuk@gmail.com>
2025-12-10 12:54:21 -0800

    Update ~/.config/nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,6 +14,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
 require("options")
+require("lazy").setup("plugins")
 require("keymaps")
