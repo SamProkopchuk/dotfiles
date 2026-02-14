0cf59d7a9dea9986570cce5c712eae1af4033c0b
Sam Prokopchuk <ssprokopchuk@gmail.com>
2026-02-14 04:53:32 -0800

    Add alias

--- .aliases.zsh
+++ .aliases.zsh
@@ -4,6 +4,7 @@ alias config='command git --git-dir="$HOME/dotfiles/" --work-tree="$HOME"'
 alias today='date +%Y%m%d'
 alias now='date +%s'
 alias mm='micromamba'
+mmake() { micromamba create -n "$1" python="$2" --use-uv -y; }
 alias ta='tmux attach || tmux new'
 alias gs='git status'
 alias gl='git log --graph --pretty=format:"%C(auto)%h %C(blue)%ar %C(green)%an%C(reset)%C(auto)%d %s" --abbrev-commit'
